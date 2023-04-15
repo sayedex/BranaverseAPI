36ce30a686ba12e213896591268cee0895f28036
ame <sayedadmod@gmail.com>
2023-04-15 15:22:12 +0600

    fixed cors

--- index.ts
+++ index.ts
@@ -18,11 +18,17 @@ const connect = async () => {
     }
   };
   const corsOptions = {
-    origin: 'http://localhost:3000', // your client's URL
-    credentials: true // enable CORS with credentials
+    origin: "*", 
+    credentials: true, // enable CORS with credentials
   };
   app.use(cors(corsOptions));
-
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
   app.use(express.json());
   app.use(cookieParser());
   app.use("/api/auth", authRoute);

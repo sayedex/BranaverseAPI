e68e29b8f508a89052e281d675f87358d3038481
ame <sayedadmod@gmail.com>
2023-04-16 19:18:08 +0600

    new

--- index.ts
+++ index.ts
@@ -16,17 +16,11 @@ const connect = async () => {
     }
   };
   const corsOptions = {
-    origin: "*", 
+    origin: "https://branaverse.vercel.app/", 
     credentials: true, // enable CORS with credentials
   };
   app.use(cors(corsOptions));
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  });
+
   app.use(express.json());
   app.use(cookieParser());
   app.use("/api/auth", authRoute);
